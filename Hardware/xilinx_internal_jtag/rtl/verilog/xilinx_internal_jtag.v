b75ef4e2a9c0601716b7bb0c42278f1a07964e02
Alex Guo <xfguo@credosemi.com.cn>
2016-02-19 15:49:46 +0800

    [wip] temp save (about artix 7 tap).
    Signed-off-by: Alex Guo <xfguo@credosemi.com.cn>

--- Hardware/xilinx_internal_jtag/rtl/verilog/xilinx_internal_jtag.v
+++ Hardware/xilinx_internal_jtag/rtl/verilog/xilinx_internal_jtag.v
@@ -456,18 +456,30 @@ assign run_test_idle_o = 1'b0;
 // The & !update_bscan tern will provide a clock edge so update_dr_o can be registered
 // The &debug_select term will drop TCK when the module is un-selected (does not happen in the BSCAN block).
 // This allows a user to kludge clock ticks in the IDLE state, which is needed by the advanced debug module.
-// !!! assign tck_o = (drck & debug_select_o & !update_bscan);
+//BUFG BUFG_tap (
+//	.I(tck),
+//	.O(tck_o)
+//);
 
 // This will hold the update_dr output so it can be registered on the rising edge
 // of the clock created above.
-always @(posedge update_bscan or posedge capture_dr_o or negedge debug_select_o)
-begin
-	if(update_bscan) update_out <= 1'b1;
-	else if(capture_dr_o) update_out <= 1'b0;
-	else if(!debug_select_o) update_out <= 1'b0;
-end
+// always @(posedge update_bscan or posedge capture_dr_o or negedge debug_select_o)
+// begin
+// 	if(update_bscan) update_out <= 1'b1;
+// 	else if(capture_dr_o) update_out <= 1'b0;
+// 	else if(!debug_select_o) update_out <= 1'b0;
+// end
+LDCE #(
+	.INIT(1'b0)
+) LDCE_tap_latch (
+	.Q(update_dr_o),
+	.CLR( (capture_dr_o) || (!debug_select_o) ),
+	.D(1'b1),
+	.G(update_bscan),
+	.GE(1'b1)
+);
 
-assign update_dr_o = update_out;
+// assign update_dr_o = update_out;
 `else
 `ifdef ARTIX7
 // Note that this version is missing two outputs.
@@ -485,7 +497,7 @@ BSCANE2 #(
     .RESET(test_logic_reset_o),   // Reset output from TAP controller
     .SEL(debug_select_o),       // USER active output
     .SHIFT(shift_dr_o),         // SHIFT output from TAP controller
-    .TCK(tck_o),
+    .TCK(tck),
     .TDI(tdi_o),                // TDI output from TAP controller
     .TMS(),
     .UPDATE(update_bscan),      // UPDATE output from TAP controller
@@ -500,10 +512,10 @@ assign run_test_idle_o = 1'b0;
 // This allows a user to kludge clock ticks in the IDLE state, which is needed by the advanced debug module.
 
 // assign tck_o = (drck & debug_select_o & !update_bscan);
-// BUFG BUFG_tap (
-// 	.I(drck & debug_select_o & (!update_bscan)),
-// 	.O(tck_o)
-// );
+BUFG BUFG_tap (
+	.I(tck),
+	.O(tck_o)
+);
 
 // This will hold the update_dr output so it can be registered on the rising edge
 // of the clock created above.
